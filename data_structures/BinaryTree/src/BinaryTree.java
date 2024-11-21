74ebd51f9cb837b11b46665368f8b5c272232bdc
KamilMad <kamilul085@gmail.com>
2024-11-21 12:20:06 +0100

    Added deletion method to BST

--- data_structures/BinaryTree/src/BinaryTree.java
+++ data_structures/BinaryTree/src/BinaryTree.java
@@ -35,7 +35,6 @@ public class BinaryTree {
     public boolean search(int data) {
         return searchHelper(this.root, data);
     }
-
     private boolean searchHelper(Node root, int data) {
         if (root == null) {
             return false;
@@ -48,5 +47,49 @@ public class BinaryTree {
         }else {
             return searchHelper(root.getRight(), data);
         }
+    }
+
+    public void delete(int data) {
+        deleteHelper(this.root, data);
+    }
+
+    public Node deleteHelper(Node root, int data) {
+        if (root == null) {
+            return root; //Find why Maybe because if is null then return null and if not then just return the value
+        }
+
+        if (data < root.getData()) {
+            root.setLeft(deleteHelper(root.getLeft(), data));
+        } else if (data > root.getData()) {
+            root.setRight(deleteHelper(root.getRight(), data));
+        } else {
+
+            if (root.getLeft() == null && root.getRight() == null) { // node have no children then return null
+                return null;
+            } else if (root.getLeft() == null){ // node have only right child then return it (node with be replaced with its right child)
+                return root.getRight();
+            } else if (root.getRight() == null){ // node have only left child then return it (node will be replaced with its left child)
+                return root.getLeft();
+            } else { // node have both children
+                Node successor = getSuccessor(root.getRight());
+                root.setData(successor.getData());
+                root.setRight(deleteHelper(root.getRight(), successor.getData()));
+            }
+        }
+        return root;
+    }
+
+    private Node getSuccessor(Node root) {
+        while (root.getLeft() != null ){
+            root = root.getLeft();
+        }
+        return root;
+    }
+
+    private Node findMin(Node root) {
+        if (root.getLeft() == null) {
+            return root;
+        }
+        return findMin(root.getLeft());
     }
 }
